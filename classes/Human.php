23318000675da512a6d9af3cf12104aeb1a9aeda
halki <s.halki0203@icloud.com>
2022-01-21 18:45:34 +0900

    Initial commit

---
+++ classes/Human.php
@@ -0,0 +1,30 @@
+<?php 
+
+class Human
+{
+
+  const MAX_HITPOINT = 100;
+  // publicはカプセル化で使用
+  public $name;
+  public $hitPoint = 100;
+  public $attackPoint = 20;
+
+  public function doAttack($enemy)
+  {
+    echo "『".$this->name."』の攻撃!!\n";
+    echo "【".$enemy->name."』に".$this->attackPoint."のダメージ!\n";
+    $enemy->tookDamage($this->attackPoint);
+  }
+
+  public function tookDamage($damage)
+  {
+    $this->hitPoint -= $damage;
+
+    if($this->hitPoint < 0) {
+      $this->hitPiont = 0;
+    }
+  }
+}
+
+
+?>
---
+++ main.php
@@ -0,0 +1,36 @@
+<?php 
+
+require_once("./classes/Human.php");
+require_once("./classes/Enemy.php");
+
+$tiida = new Human();
+$goblin = new Enemy();
+
+$tiida->name = "ティーダ";
+$goblin->name = "ゴブリン";
+
+echo $tiida->name."\n";
+echo $goblin->name."\n";
+
+$turn = 1;
+
+while($tiida->hitPoint > 0 && $goblin->hitPoint > 0){
+  echo "*** $turn ターン目 ***\n\n";
+
+  echo $tiida->name.":".$tiida->hitPoint."/".$tiida::MAX_HITPOINT."\n";
+  echo $goblin->name.":".$goblin->hitPoint."/".$goblin::MAX_HITPOINT."\n";
+
+  $tiida->doAttack($goblin);
+  echo "\n";
+  $goblin->doAttack($tiida);
+  echo "\n";
+
+  $turn++;
+
+}
+
+echo "★★★ 戦闘終了 ★★★\n\n";
+echo $tiida->name . "　：　" . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+echo $goblin->name . "　：　" . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
+
+?>
